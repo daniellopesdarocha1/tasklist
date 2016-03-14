f6e8e4362f7c767af79011f05cd59d3355dbdfc3
Daniel Lopes da Rocha <daniellopesdarocha1@gmail.com>
2016-03-14 16:30:07 -0300

    Criando banco no MongoDB para guardar as tarefas e listar com programação reativa

--- client/lista/lista.js
+++ client/lista/lista.js
@@ -1,10 +1,5 @@
 Template.lista.helpers({
 	tarefas : function(){
-		return [
-			{nome: "Trabalho de Lab de Eng. Soft"},
-			{nome: "Estudar JavaScript"},
-			{nome: "Estudar MongoDB"},
-			{nome: "Documentação TCC"}
-		];
+		return Tarefas.find({});
 	}
 });
