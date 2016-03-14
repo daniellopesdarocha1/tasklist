c418a2274f30a890f008695a29b1bb6f8413e91f
Daniel Lopes da Rocha <daniellopesdarocha1@gmail.com>
2016-03-14 16:11:43 -0300

    Criando componente lista e helper para tarefas

---
+++ client/lista/lista.js
@@ -0,0 +1,10 @@
+Template.lista.helpers({
+	tarefas : function(){
+		return [
+			{nome: "Trabalho de Lab de Eng. Soft"},
+			{nome: "Estudar JavaScript"},
+			{nome: "Estudar MongoDB"},
+			{nome: "Documentação TCC"}
+		];
+	}
+});
